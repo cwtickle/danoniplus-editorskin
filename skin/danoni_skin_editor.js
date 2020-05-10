72fd7fb6ab3cc368241dce88702d36a5148a2739
tickle <qu7.52m@gmail.com>
2020-05-10 11:38:12 +0900

    [ver0.1.0] 画面移動系の関数を追加

--- skin/danoni_skin_editor.js
+++ skin/danoni_skin_editor.js
@@ -5,12 +5,12 @@
  * 
  * Source by tickle
  * Created : 2020/05/09
- * Revised :
+ * Revised : 2020/05/10
  * 
  * https://github.com/cwtickle/danoniplus-editorskin
  */
-const g_editorVersion = `Ver 0.0.0`;
-const g_editorRevisedDate = `2020/05/09`;
+const g_editorVersion = `Ver 0.1.0`;
+const g_editorRevisedDate = `2020/05/10`;
 
 /**
  * タイトル画面 [Scene: Title / Melon]
@@ -43,14 +43,66 @@ function skinOptionInit() {
 }
 
 /**
- * エディター子画面の作成
+ * エディター用の画面クリア関数
+ */
+function clearEditorWindow() {
+    deleteChildspriteAll(`editorRoot`);
+}
+
+/**
+ * エディタータイトル画面の作成
  */
 function createEditorWindow() {
     const editorRoot = createSprite(`divRoot`, `editorRoot`, 0, 0, g_sWidth, g_sHeight);
     editorRoot.style.background = `#ffffff`;
-
     editorRoot.appendChild(createImg(`editorTitle`, `../img/editor/title.png`, g_sWidth / 2 - 150, 20, 300, 110));
 
+    // スタートボタン描画
+    const btnNew = createCssButton({
+        id: `btnNew`,
+        name: `新規作成 New`,
+        x: g_sWidth / 4,
+        y: g_sHeight - 125,
+        width: g_sWidth / 2,
+        height: C_BTN_HEIGHT / 2,
+        fontsize: C_LBL_BTNSIZE * 2 / 3,
+        align: C_ALIGN_CENTER,
+        class: g_cssObj.button_Next,
+    }, _ => {
+        clearEditorWindow();
+        mainEditorInit();
+    });
+    editorRoot.appendChild(btnNew);
+
+    // ロードボタン描画
+    const btnLoad = createCssButton({
+        id: `btnLoad`,
+        name: `ふっかつのじゅもん Load`,
+        x: g_sWidth / 4,
+        y: g_sHeight - 90,
+        width: g_sWidth / 2,
+        height: C_BTN_HEIGHT / 2,
+        fontsize: C_LBL_BTNSIZE * 2 / 3,
+        align: C_ALIGN_CENTER,
+        class: g_cssObj.button_Setting,
+    }, _ => {
+        clearEditorWindow();
+        loadEditorInit();
+    });
+    editorRoot.appendChild(btnLoad);
+
+    const lnkEVersion = createCssButton({
+        id: `lnkVersion`,
+        name: `&copy; ${g_revisedDate.slice(0, 4)} ティックル, CW-Editor ${g_editorVersion}`,
+        x: 10,
+        y: g_sHeight - 20,
+        width: g_sWidth - 20,
+        height: 16,
+        fontsize: 12,
+        align: C_ALIGN_RIGHT,
+        class: g_cssObj.button_Tweet,
+    }, _ => window.open(`https://github.com/cwtickle/danoniplus-editorskin`, `_blank`));
+    editorRoot.appendChild(lnkEVersion);
 
     // エディターを閉じる
     const btnEditorClose = createCssButton({
@@ -64,10 +116,61 @@ function createEditorWindow() {
         align: C_ALIGN_CENTER,
         class: g_cssObj.button_Start,
     }, _ => {
-        divRoot.removeChild(editorRoot);
-        divRoot.removeChild(btnEditorClose);
+        if (window.confirm(`エディターを終了します。よろしいですか？`)) {
+            divRoot.removeChild(editorRoot);
+            divRoot.removeChild(btnEditorClose);
+        }
     });
-    divRoot.appendChild(btnEditorClose);
+    editorRoot.appendChild(btnEditorClose);
+}
+
+/**
+ * ロード画面
+ */
+function loadEditorInit() {
+
+    // タイトルへ戻る
+    const btnEditorBack = createCssButton({
+        id: `btnEditorBack`,
+        name: `<`,
+        x: 10,
+        y: 10,
+        width: 30,
+        height: 30,
+        fontsize: 20,
+        align: C_ALIGN_CENTER,
+        class: g_cssObj.button_Back,
+    }, _ => {
+        clearEditorWindow();
+        createEditorWindow();
+    });
+    editorRoot.appendChild(btnEditorBack);
+
+}
+
+/**
+ * メイン画面
+ */
+function mainEditorInit() {
+
+    // タイトルへ戻る
+    const btnEditorBack = createCssButton({
+        id: `btnEditorBack`,
+        name: `<`,
+        x: 10,
+        y: 10,
+        width: 30,
+        height: 30,
+        fontsize: 20,
+        align: C_ALIGN_CENTER,
+        class: g_cssObj.button_Back,
+    }, _ => {
+        if (window.confirm(`エディターを終了します。よろしいですか？`)) {
+            clearEditorWindow();
+            createEditorWindow();
+        }
+    });
+    editorRoot.appendChild(btnEditorBack);
 }
 
 /**
